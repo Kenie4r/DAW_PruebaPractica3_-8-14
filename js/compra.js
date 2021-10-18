0635b3af228df8351f3d33d4e58ccdcf0b259a87
Diego Mancía <62128556+DiegoSan03@users.noreply.github.com>
2021-10-18 12:25:49 -0600

    compra update

--- js/compra.js
+++ js/compra.js
@@ -1,54 +1,59 @@
 /* DATOS PELICULAS */
 /* A partir de este objeto JSON obtenemos los datos de las peliculas en cartelera */
-const peliculas = {
+const peliculas  = {
     "Pelicula1" : {
         "Nombre": "Venom 2: Habrá matanza",
         "Duracion": "2 horas" ,
         "Precio" : "2.53",
         "Clasificacion" : "+12",
-        "TipoB": "Premiun", 
+        "TipoB": "Ecoamigable", 
         "Horarios": {
             "1": "4:30" , "2": "6:30", "3": "8:30"
-        } 
+        },
+        "img" : "img/venom2.jpg"
     }, "Pelicula2" : {
         "Nombre": "BlackWidown",
         "Duracion": "2 horas" ,
         "Precio" : "3",
         "Clasificacion" : "+12",
-        "TipoB": "Premiun", 
+        "TipoB": "Normal", 
         "Horarios": {
             "1": "5:00" , "2": "7:00", "3": "9:00"
-        } 
+        },
+        "img" : "img/blackwidow.jpg"
     },
     "Pelicula3" : {
         "Nombre": "Spider-Man: No way Home",
         "Duracion": "3 horas" ,
         "Precio" : "4",
         "Clasificacion" : "+12",
-        "TipoB": "Premiun", 
+        "TipoB": "Cuero exportado", 
         "Horarios": {
             "1": "3:30" , "2": "6:30", "3": "9:30"
-        } 
+        },
+        "img": "img/spirderman.jpg"
     },
     "Pelicula4" : {
         "Nombre": "Batman",
         "Duracion": "2 horas" ,
         "Precio" : "4",
         "Clasificacion" : "+18",
-        "TipoB": "Premiun", 
+        "TipoB": "Cuero exportado", 
         "Horarios": {
             "1": "2:30" , "2": "4:30", "3": "6:30"
-        } 
+        }, 
+        "img": "img/batman.jpg"
     },
     "Pelicula5" : {
         "Nombre": "Doctor Strange: En el multiverso de la locura",
         "Duracion": "2 horas" ,
         "Precio" : "4",
         "Clasificacion" : "+12",
-        "TipoB": "Premiun", 
+        "TipoB": "Plastico resistente", 
         "Horarios": {
             "1": "3:30" , "2": "5:30", "3": "7:30"
-        } 
+        }, 
+        "img": "img/ds.jpg"
     }
     
 }
@@ -118,29 +123,44 @@ function enviarFormulario() {
 function llenarPelicula(idPelicula) {
     var titulo = document.getElementById("titulo-pelicula");
     var poster = document.getElementById("poster-pelicula");
+    var butaca = document.getElementById("tipoButaca");
 
     switch( idPelicula ){
         case "1":
             titulo.innerHTML = peliculas.Pelicula1.Nombre;
-            poster.src = "../img/venom2.jpg";
+            poster.src = "../" + peliculas.Pelicula1.img;
             llenarHorario(peliculas.Pelicula1.Horarios);
+            butaca.innerText = peliculas.Pelicula1.TipoB;
             break;
         case "2":
             titulo.innerHTML = peliculas.Pelicula2.Nombre;
+            poster.src = "../" + peliculas.Pelicula2.img;
+            llenarHorario(peliculas.Pelicula2.Horarios);
+            butaca.innerText = peliculas.Pelicula2.TipoB;
             break;
         case "3":
             titulo.innerHTML = peliculas.Pelicula3.Nombre;
+            poster.src = "../" + peliculas.Pelicula3.img;
+            llenarHorario(peliculas.Pelicula3.Horarios);
+            butaca.innerText = peliculas.Pelicula3.TipoB;
             break;
         case "4":
             titulo.innerHTML = peliculas.Pelicula4.Nombre;
+            poster.src = "../" + peliculas.Pelicula4.img;
+            llenarHorario(peliculas.Pelicula4.Horarios);
+            butaca.innerText = peliculas.Pelicula4.TipoB;
             break;
         case "5":
             titulo.innerHTML = peliculas.Pelicula5.Nombre;
+            poster.src = "../" + peliculas.Pelicula5.img;
+            llenarHorario(peliculas.Pelicula5.Horarios);
+            butaca.innerText = peliculas.Pelicula5.TipoB;
             break;
         default:
             titulo.innerHTML = peliculas.Pelicula1.Nombre;
             poster.src = "../img/venom2.jpg";
             llenarHorario(peliculas.Pelicula1.Horarios);
+            butaca.innerText = peliculas.Pelicula1.TipoB;
             break;
     }
 }
